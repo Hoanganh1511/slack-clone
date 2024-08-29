979b6fe21ed61c1c5408b8de5875e6ba33a684d3
Hoang Tuan Anh <hoanganhss2k@gmail.com>
2024-08-29 17:57:17 +0700

    update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,11 +3,14 @@ import { GoogleIcon } from "../icons/google";
 import Search from "./search-header";
 import Navigation from "./navigation";
 import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full px-[24px] h-[52px] bg-white">
       <div className="flex items-center w-[1128px] min-h-[100%] mx-auto">
-        <FaLinkedin className="w-[40px] h-[40px] text-[#0a66c2]" />
+        <Link href="/">
+          <FaLinkedin className="w-[40px] h-[40px] text-[#0a66c2]" />
+        </Link>
         <Search />
         <Navigation />
       </div>
